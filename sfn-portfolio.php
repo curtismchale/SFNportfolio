ca58bc63d574b8daea82ab8899972c4938a84ba3
Curtis McHale <curtis@curtismchale.ca>
2010-08-29 08:22:40 -0700

    setting up basic file structure

---
+++ sfn-portfolio.php
@@ -0,0 +1,40 @@
+<?php
+
+    /*
+        Plugin Name: SFNevents
+        Plugin URI: 
+        Description: Builds a custom post type named 'event' and gives multiple view options that may be needed
+        Version: 0.1
+        Author: Curtis McHale
+        Author URI: http://curtismchale.ca
+        License: GPL2 
+
+        Copyright 2010  PLUGIN_AUTHOR_NAME  (email : curtis@curtismchale.ca)
+
+        This program is free software; you can redistribute it and/or modify
+        it under the terms of the GNU General Public License, version 2, as 
+        published by the Free Software Foundation.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program; if not, write to the Free Software
+        Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+    */
+
+?>
+<?php
+
+    // includes the custom post type
+    require_once(TEMPLATEPATH . '/assets/includes/sfn-portfolio-custom-post-type.php');
+
+    // includes the custom taxonomy
+    require_once(TEMPLATEPATH . '/assets/includes/sfn-portfolio-custom-taxonomies.php');
+
+    // differing views with shortcodes come from here
+    require_once(TEMPLATEPATH . '/assets/includes/sfn-portfolio-custom-views.php');
+
+?>
